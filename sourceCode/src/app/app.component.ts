4c1e975ecfbf63470b2b696e572159a68bedec57
christylueis <christy.lueis@triestedigital.com>
2025-02-12 15:46:18 +0530

    test ui

--- sourceCode/src/app/app.component.ts
+++ sourceCode/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomrComponent } from './components/homr/homr.component';
+import { ProgressSpinnerComponent } from './components/progress-spinner/progress-spinner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomrComponent],
+  imports: [RouterOutlet,HomrComponent,ProgressSpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
